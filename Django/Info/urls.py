9601f00b1e06180f771cbdae8d0d6318b32bd27f
M.Amiri <meisamamiri2020@gmail.com>
2025-09-11 20:06:36 +0200

    Update urls.py

--- Django/Info/urls.py
+++ Django/Info/urls.py
@@ -1,5 +1,7 @@
+
 from django.urls import path
 from .views import *
+
 urlpatterns = [
     path("memory",  MemoryInfoView.as_view(),name="memory"),
     path("cpu", CpuInfoView.as_view(),name="cpu"),
